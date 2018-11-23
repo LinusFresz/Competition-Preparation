cb6979a08b8cba987a30d0e2dc02adfaaa3a655d
Linus Frész <LinusFresz@users.noreply.github.com>
2018-11-23 18:02:44 +0100

    Catch DB connection errors

--- db/Database.py
+++ db/Database.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 
-import pymysql
+import pymysql, sys
 from pymysql.cursors import Cursor
 
 
 class Database:
     def __init__(self, db: str, host: str, user: str, passwd: str, port: int = 3306, socket: str = None):
         if socket is None:
-            self.cnx = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, autocommit=True)
+            try: 
+                self.cnx = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, autocommit=True)
+            except pymysql.Error as error:
+                print('An error occured while connecting to the WCA database:')
+                print(error)
+                print('\n Script aborted.')
+                sys.exit()
         else:
-            self.cnx = pymysql.connect(host=host, unix_socket=socket, user=user, passwd=passwd, db=db, autocommit=True)
+            try:
+                self.cnx = pymysql.connect(host=host, unix_socket=socket, user=user, passwd=passwd, db=db, autocommit=True)
+            except pymysql.Error as error:
+                print('An error occured while connecting to the WCA database:')
+                print(error)
+                print('\n Script aborted.')
+                sys.exit()
 
     def begin_transaction(self):
         self.cnx.autocommit(False)
